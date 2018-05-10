87e0e82999a6d2146bf02f0d0f40319a3a62bd39
juin1997 <gggg704@live.com>
2018-05-10 11:51:56 +0800

    web application using entityframeworkcore

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        private DataContext context;
+        public HomeController(DataContext ctx) => context = ctx;
+        public IActionResult Index() => View();
+        public IActionResult Respond() => View();
+        [HttpPost] public IActionResult Respond(GuestResponse response) { context.Responses.Add(response); context.SaveChanges(); return RedirectToAction(nameof(Thanks), new { Name = response.Name, WillAttend = response.WillAttend }); }
+        public IActionResult Thanks(GuestResponse response) => View(response);
+        public IActionResult ListResponses() => View(context.Responses.Where(r => r.WillAttend == true).OrderBy(r=>r.Email));
+    }
+}
---
+++ PartyInvites/PartyInvites/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PartyInvites.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<GuestResponse> Responses { get; set; }
+    }
+}
